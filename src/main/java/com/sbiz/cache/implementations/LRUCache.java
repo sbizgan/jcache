1d4ac8bc5b1983d825257f76d6e7dfc26d7de511
Silviu Bizgan <sbizgan@gmail.com>
2018-03-18 18:37:38 +0200

    LRU Small update

--- src/main/java/com/sbiz/cache/implementations/LRUCache.java
+++ src/main/java/com/sbiz/cache/implementations/LRUCache.java
@@ -65,38 +65,38 @@ public class LRUCache<K, V> extends ACache<K, V> {
     }
 
     public V get(K key) {
-        Node<K, V> tempNode = cache.get(key);
-        if (tempNode == null) {
+        Node<K, V> currentNode = cache.get(key);
+        if (currentNode == null) {
             return null;
         }
         // If MRU leave the list as it is
-        else if (tempNode.key == mostRecentlyUsed.key) {
+        else if (currentNode.key == mostRecentlyUsed.key) {
             return mostRecentlyUsed.value;
         }
 
         // Get the next and previous nodes
-        Node<K, V> nextNode = tempNode.next;
-        Node<K, V> previousNode = tempNode.previous;
+        Node<K, V> nextNode = currentNode.next;
+        Node<K, V> previousNode = currentNode.previous;
 
         // If at the left-most, we update LRU 
-        if (tempNode.key == leastRecentlyUsed.key) {
+        if (currentNode.key == leastRecentlyUsed.key) {
             nextNode.previous = null;
             leastRecentlyUsed = nextNode;
         }
 
         // If we are in the middle, we need to update the items before and after our item
-        else if (tempNode.key != mostRecentlyUsed.key) {
+        else if (currentNode.key != mostRecentlyUsed.key) {
             previousNode.next = nextNode;
             nextNode.previous = previousNode;
         }
 
         // Finally move our item to the MRU
-        tempNode.previous = mostRecentlyUsed;
-        mostRecentlyUsed.next = tempNode;
-        mostRecentlyUsed = tempNode;
+        currentNode.previous = mostRecentlyUsed;
+        mostRecentlyUsed.next = currentNode;
+        mostRecentlyUsed = currentNode;
         mostRecentlyUsed.next = null;
 
-        return tempNode.value;
+        return currentNode.value;
     }
 
     public boolean containsKey(K key) {
